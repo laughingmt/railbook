19e39b967e8ac61a683df460e09cf85a96ff00f7
Hiroshi Yamada <mutable0827@gmail.com>
2013-10-26 15:32:33 +0900

    First Book

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131025084748) do
+ActiveRecord::Schema.define(:version => 20131026062944) do
 
   create_table "books", :force => true do |t|
     t.string   "isbn"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(:version => 20131025084748) do
     t.string   "publish"
     t.date     "published"
     t.boolean  "cd"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
